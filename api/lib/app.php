9f1d4a463f638a4c64864757cce958fc44e53880
gt4wxx <ellissonsantos403@gmail.com>
2026-08-30 11:35:10 -0300

    vamos

--- api/lib/app.php
+++ api/lib/app.php
@@ -2,7 +2,7 @@
 
 function saidera_v(): string
 {
-    return '39';
+    return '40';
 }
 
 function saidera_https(): bool
--- assets/js/ui.js
+++ assets/js/ui.js
@@ -9,7 +9,7 @@ const Brand = {
     return this.pages() ? "../Saidera_Kit_Marca" : "Saidera_Kit_Marca";
   },
   v() {
-    return String(window.SAIDERA_V || "39");
+    return String(window.SAIDERA_V || "40");
   },
   cache(url) {
     if (!url || /[?&]v=/.test(url)) return url;
